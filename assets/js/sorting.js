484661b60a4b4d17d4c7acb7a8c9cd450b5f63c3
danielbbastos <danieljoao5@gmail.com>
2018-06-20 17:50:26 +0100

    Sorting table changed to not allow multiselection

--- assets/js/sorting.js
+++ assets/js/sorting.js
@@ -6,14 +6,15 @@ else {
 	var tagsArray = JSON.parse(sessionStorage.getItem("tagsArray"));
 }
 
+// Sorting table multiselection
+const MULTISELECTION = false;
+
 
 
 function filter(tag) {
-	if(tagsArray.indexOf(tag) <= -1)
-    tagsArray.push(tag);
-  setActiveTag(tag);
-  showContainer();
-	sessionStorage.setItem("tagsArray", JSON.stringify(tagsArray));
+    setActiveTag(tag);
+    showContainer();
+    sessionStorage.setItem("tagsArray", JSON.stringify(tagsArray));
 }
 
 
@@ -21,26 +22,57 @@ function filter(tag) {
 
 
 function setActiveTag(tag) {
-  //if not active, set tag to active, else remove active
+    // deselects the all tags button
 	document.getElementById('all-posts').classList.remove('active');
-  var item = document.getElementById(tag + '-item');
-  if(item) {
-    if(item.classList.contains('active')) {
-			item.classList.remove('active');
-			var index = tagsArray.indexOf(tag);
-			if (index > -1) {
-    		tagsArray.splice(index, 1);
-			}
+    // gets the tag button
+    var item = document.getElementById(tag + '-item');
+
+    if(item) {
+        // if we allow tag multiselection
+        if(MULTISELECTION) {
+            // the deselect button case - clicking in an active button
+            if(item.classList.contains('active')) {
+                item.classList.remove('active');
+                var index = tagsArray.indexOf(tag);
+                if (index > -1) {
+                    // remove the tag from memory
+                    tagsArray.splice(index, 1);
+                }
+            }
+            // the select button case - clicking an unselected button
+            else {
+                // if memory doesn't have the tag, add it
+                if(tagsArray.indexOf(tag) <= -1) {
+                    tagsArray.push(tag);
+                }
+                item.classList.add('active');
+            }
+        }
+        // if we don't allow tag multiselection
+        else {
+            // the deselect button case - clicking in an active button
+            if(item.classList.contains('active')) {
+                order("chrono-date");
+            }
+            // the select button case - clicking an unselected button
+            else {
+                //loops through all tags and removes the active class
+                var items = document.getElementsByClassName('blog-tag-item');
+                for(var i=0; i < items.length; i++) {
+                    items[i].setAttribute('class', 'blog-tag-item');
+                }
+                item.classList.add('active');
+                tagsArray = [tag];
+                sessionStorage.setItem("tagsArray", JSON.stringify(tagsArray));
+            }
+        }
     }
-		else {
-    	item.classList.add('active');
-		}
-  }
+
 
  	//displays a message on sorting table of tags being used
-  if(tagsArray.length === 1){
+    if(tagsArray.length === 1){
 		var text = "Showing posts containing the tag <b>" + tagsArray[0] + "</b>.";
-  }
+    }
 	else {
 		var text = "Showing posts containing the tags ";
 		for(i=0; i < tagsArray.length; i++){
@@ -56,7 +88,7 @@ function setActiveTag(tag) {
 		}
 	}
 	var elem = document.getElementsByClassName('selected-tags-text');
-  elem[0].innerHTML = text;
+    elem[0].innerHTML = text;
 }
 
 
@@ -65,22 +97,22 @@ function setActiveTag(tag) {
 
 
 function showContainer() {
-  // loop through all lists and hide them
-  var lists = document.getElementsByClassName('post-list-item');
-  for(var i=0; i < lists.length; i++) {
-    lists[i].classList.add('hidden');
-  }
-
-  // remove the hidden class from all the tags selected on the table
-	if(tagsArray.length > 0) {
-    var list = document.getElementsByClassName(tagsArray.join(" ").toString());
-    for(i = 0; i < list.length; i++) {
-      list[i].classList.remove('hidden');;
+    // loop through all posts and hide them
+    var lists = document.getElementsByClassName('post-list-item');
+    for(var i=0; i < lists.length; i++) {
+        lists[i].classList.add('hidden');
     }
-	}
-	else {
-		order("chrono-date");
-	}
+
+    // remove the hidden class from all the tags selected on the table
+    if(tagsArray.length > 0) {
+        var list = document.getElementsByClassName(tagsArray.join(" ").toString());
+        for(i = 0; i < list.length; i++) {
+          list[i].classList.remove('hidden');;
+        }
+    }
+    else {
+    	order("chrono-date");
+    }
 }
 
 
@@ -89,22 +121,22 @@ function showContainer() {
 
 function order(type) {
 	//loops through all tags and removes the active class
-  var items = document.getElementsByClassName('blog-tag-item');
-  for(var i=0; i < items.length; i++) {
-    items[i].setAttribute('class', 'blog-tag-item');
-  }
-	//update sorting info
-  var tag = document.getElementsByClassName('selected-tags-text');
-  tag[0].innerHTML = "Showing <b>all</b> posts ordered by date.";
+    var items = document.getElementsByClassName('blog-tag-item');
+    for(var i=0; i < items.length; i++) {
+        items[i].setAttribute('class', 'blog-tag-item');
+    }
 
 	//shows all posts in chronological order
-  if(type === "chrono-date") {
-		document.getElementById('all-posts').classList.add('active');
-		var lists = document.getElementsByClassName('post-list-item');
-		for(var i=0; i < lists.length; i++) {
-      lists[i].classList.remove('hidden');
+    if(type === "chrono-date") {
+    	document.getElementById('all-posts').classList.add('active');
+    	var lists = document.getElementsByClassName('post-list-item');
+    	for(var i=0; i < lists.length; i++) {
+            lists[i].classList.remove('hidden');
+        }
+        //update sorting info
+        var tag = document.getElementsByClassName('selected-tags-text');
+        tag[0].innerHTML = "Showing <b>all</b> posts ordered by date.";
     }
-  }
 
 	tagsArray = [];
 	sessionStorage.setItem("tagsArray", JSON.stringify(tagsArray));
@@ -116,7 +148,9 @@ function order(type) {
 
 
 function updateTags() {
+    // hide all posts
 	document.getElementById('all-posts').classList.remove('active');
+
 	for(i=0; i < tagsArray.length; i++) {
 		var tag = tagsArray[i];
 		var item = document.getElementById(tag + '-item');
@@ -171,6 +205,7 @@ window.onload = function () {
 		sessionStorage.setItem("tagsArray", JSON.stringify(tagsArray));
     var tag = window.location.hash.split('#')[1];
     var sortTable = document.getElementsByClassName('selected-tags-text');
+    // to scroll down to text on page load
     sortTable[0].setAttribute('id', tag);
     filter(tag);
     document.location="#"+tag;
